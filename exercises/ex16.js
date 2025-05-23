ecd17150707fa9b187aa7d24f9bb4e2f280ee857
ellieCorner <ellie25corner@gmail.com>
2025-05-22 19:28:20 -0700

    exercise 16 completed

--- exercises/ex16.js
+++ exercises/ex16.js
@@ -29,9 +29,19 @@ This will be the most complicated program you've written yet, so take some time 
 */
 
 function makeLine(length) {
-    var line = "";
-    for (var j = 1; j <= length; j++) {
-      line += "* "
-    }
-    return line + "\n";
+  var line = "";
+  for (var j = 1; j <= length; j++) {
+    line += "* ";
+  }
+  return line + "\n";
 }
+
+function buildTriangle(num) {
+  let result = "";
+  for (let i = 1; i <= num; i++) {
+    result += makeLine(i);
+  }
+  return result;
+}
+
+console.log(buildTriangle(10));
