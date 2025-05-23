0dfec895eaa0ed246a7c39abbf7997d376d1fb8a
ellieCorner <ellie25corner@gmail.com>
2025-05-22 19:29:16 -0700

    exercise 28 completed

--- exercises/ex28.js
+++ exercises/ex28.js
@@ -11,3 +11,38 @@ deleteMessage(index) - removes the message corresponding to the index provided
 addFriend() - increases the friend count by 1
 removeFriend() - decreases the friend count by 1
 */
+
+const facebookProfile = {
+  name: "Ellie",
+  friendCounter: 0,
+  posts: [],
+  postMessage: function (message) {
+    this.posts.push(message);
+  },
+  deleteMessage: function (index) {
+    if (index >= 0 && index < this.posts.length) {
+      this.posts.splice(index, 1);
+    } else {
+      console.log("Message not found.");
+    }
+  },
+  addFriend: function () {
+    this.friendCounter++;
+  },
+  removeFriend: function () {
+    if (this.friendCounter > 0) {
+      this.friendCounter--;
+    } else {
+      console.log("There's no friends");
+    }
+  },
+};
+
+facebookProfile.postMessage("Hello");
+console.log(facebookProfile);
+facebookProfile.deleteMessage(0);
+console.log(facebookProfile);
+facebookProfile.addFriend();
+console.log(facebookProfile);
+facebookProfile.removeFriend();
+console.log(facebookProfile);
