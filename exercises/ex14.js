0a72a9bd7d082a91bb4ce8c70ae4881d891842ee
ellieCorner <ellie25corner@gmail.com>
2025-05-22 19:28:12 -0700

    exercise 14 completed

--- exercises/ex14.js
+++ exercises/ex14.js
@@ -11,3 +11,13 @@ row: 25 - seat: 97
 row: 25 - seat: 98
 row: 25 - seat: 99
 */
+
+function printSeat(maxRow, maxSeat) {
+  for (let row = 0; row < maxRow; row++) {
+    for (let seat = 0; seat < maxSeat; seat++) {
+      console.log(`row: ${row} - seat: ${seat}`);
+    }
+  }
+}
+
+printSeat(25, 100);
