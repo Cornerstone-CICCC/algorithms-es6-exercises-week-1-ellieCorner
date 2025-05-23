4850031c54b7fb8474e235a00f16ce4ef8fcaba3
ellieCorner <ellie25corner@gmail.com>
2025-05-22 19:28:42 -0700

    exercise 21 completed

--- exercises/ex21.js
+++ exercises/ex21.js
@@ -6,3 +6,11 @@ TIP: The 1st element of any array has an index of 0.
 
 Afterwards, print out the prices array to the console.
 */
+
+var prices = [1.23, 48.11, 90.11, 8.5, 9.99, 1.0, 1.1, 67.0];
+
+prices[0] = 5.12;
+prices[2] = 10.04;
+prices[6] = 15.23;
+
+console.log(prices);
