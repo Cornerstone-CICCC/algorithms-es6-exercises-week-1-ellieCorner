5ea3532381f7762359091a22c8ed5c8f5f01d245
ellieCorner <ellie25corner@gmail.com>
2025-05-22 19:27:14 -0700

    exercise 2 completed

--- exercises/ex02.js
+++ exercises/ex02.js
@@ -10,3 +10,21 @@ Write a series of conditional statements that:
 
 */
 
+function printTeamName(num) {
+  switch (num) {
+    case 0:
+      return console.log("not a group");
+    case 1:
+      return console.log("solo");
+    case 2:
+      return console.log("duet");
+    case 3:
+      return console.log("trio");
+    case 4:
+      return console.log("quartet");
+    default:
+      return console.log("this is a large group");
+  }
+}
+
+printTeamName(3);
